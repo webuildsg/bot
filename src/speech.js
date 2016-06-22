ec7e2e6c3eddf4f60de6638fcc4fd67c0c61eb36
KahWee Teng <t@kw.sg>
2016-06-21 19:26:26 -0700

    Support "meetup" on top of "events"

--- src/speech.js
+++ src/speech.js
@@ -6,14 +6,16 @@ exports.getParsedRequest = function getParsedRequest (text) {
   let dates = nlpText.dates()
   const root = nlpText.root()
   let output = {
-    mode: root.includes('event') ? 'event' : root.includes('repo') ? 'repo' : undefined,
+    mode: root.includes('event') || root.includes('meetup') ? 'event' : root.includes('repo') ? 'repo' : undefined,
     upcoming: root.includes('upcoming')
   }
   if (dates.length >= 1) {
     output.dates = dates
       .map((date) => date.data)
       .reduce((prev, curr) => {
-        Object.keys(prev).forEach((d) => curr[d] = curr[d] || prev[d])
+        Object.keys(prev).forEach((d) => {
+          curr[d] = curr[d] || prev[d]
+        })
         return curr
       }, {year: null, month: null, day: null})
     output.dates.year = output.dates.year || (new Date()).getUTCFullYear()
--- test/speech.js
+++ test/speech.js
@@ -9,6 +9,9 @@ describe('speech', function () {
       expect(getParsedRequest('some repos please').mode).to.equal('repo')
       expect(getParsedRequest('some repository please').mode).to.equal('repo')
     })
+  })
+
+  describe('getParsedRequest date', function () {
     it('should detect if there is a date', function () {
       // No date means undefined
       expect(getParsedRequest('testing').dates).to.be.undefined
@@ -20,6 +23,7 @@ describe('speech', function () {
       expect(getParsedRequest('4-july').dates.year).to.equal((new Date()).getUTCFullYear())
       expect(getParsedRequest('july 4th 2020').dates.year).to.equal(2020)
       expect(getParsedRequest('I married April for the 2nd time on June 5th 1998').dates.year).to.equal(1998)
+      expect(getParsedRequest('upcoming events on jun 23rd').dates.month).to.equal(5)
     })
   })
 })
