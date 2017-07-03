f62c190f9601cfea95ecff4347c540955c1c04f9
KahWee Teng <t@kw.sg>
2017-07-03 16:49:37 -0700

    ignore .git

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'index',
     script: './index.js',
     watch: true,
-    ignore_watch: ['uploads', 'data', 'data/users'],
+    ignore_watch: ['uploads', 'data', 'data/users', '.git'],
     env: {
       'NODE_ENV': 'development'
     },
